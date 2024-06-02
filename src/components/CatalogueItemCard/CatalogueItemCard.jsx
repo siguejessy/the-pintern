12ff17888f704bc0d0b9232cd1fa4ab140f3fd59
Jessica <jessicamendez328@gmail.com>
2024-06-02 17:01:30 -0600

    tailwind init

---
+++ src/components/CatalogueItemCard/CatalogueItemCard.jsx
@@ -0,0 +1,45 @@
+
+export default function CatalogueItemCard({ catalogueItems }) {
+ 
+  const items = catalogueItems.map(item =>
+    <CatalogueListItem
+      key={item._id}
+      catalogueItem={item}
+      handleViewDetails={handleViewDetails}
+    />
+  );
+  
+  return (
+    <div className="bg-white">
+      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+        <h2 className="text-2xl font-bold tracking-tight text-gray-900">Customers also purchased</h2>
+
+        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+          {catalogueItems.map((item) => (
+            <div key={item.id} className="group relative">
+              <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                <img
+                  src={item.imageSrc}
+                  alt={item.imageAlt}
+                  className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                />
+              </div>
+              <div className="mt-4 flex justify-between">
+                <div>
+                  <h3 className="text-sm text-gray-700">
+                    <a href={item.href}>
+                      <span aria-hidden="true" className="absolute inset-0" />
+                      {item.name}
+                    </a>
+                  </h3>
+                  <p className="mt-1 text-sm text-gray-500">{item.variant}</p>
+                </div>
+                <p className="text-sm font-medium text-gray-900">{item.price}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
