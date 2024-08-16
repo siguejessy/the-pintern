0ff331e271bf6ac2df8cd1df9f17d5731b5541cc
Jessica <jessicamendez328@gmail.com>
2024-08-16 17:10:33 -0400

    config files for heroku deployment

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
+import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
+
 
 export default defineConfig(({ command, mode }) => {
   // Load env file based on `mode` in the current working directory.
@@ -8,6 +11,11 @@ export default defineConfig(({ command, mode }) => {
 
   return {
     base: './',
+    resolve: {
+      alias: {
+        '~': path.resolve(__dirname, './src'),
+      },
+    },
     define: {
       __APP_ENV__: JSON.stringify(env.APP_ENV),
     },
