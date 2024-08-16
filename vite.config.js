e327a291a4a53cab8120c613330dd5f4b13dab7b
Jessica <jessicamendez328@gmail.com>
2024-08-16 17:26:43 -0400

    config files for heroku deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
+import tailwindcss from "tailwindcss";
 
 
 export default defineConfig(({ command, mode }) => {
@@ -29,6 +30,11 @@ export default defineConfig(({ command, mode }) => {
           'src/**/*.svg',
         ],
       }),
+      css: {
+        postcss: {
+        plugins: [tailwindcss()],
+        },
+      },
     ],
     assetsInclude: ['**/*.m4v'], //irrelevant for now, keeping for future reference, might help for importing into components
   };
